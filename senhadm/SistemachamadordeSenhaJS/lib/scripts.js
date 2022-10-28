346ba9890abe0e83a89e5baf0f86831ab59f50f4
snow-sr <joaofelipi877@gmail.com>
2022-10-28 17:55:04 -0300

    terminei

--- senhadm/SistemachamadordeSenhaJS/lib/scripts.js
+++ senhadm/SistemachamadordeSenhaJS/lib/scripts.js
@@ -10,17 +10,34 @@ jQuery(document).ready(function ($) {
     var senhaNormal = $("#senhaNormal");
     var senhaPrior = $("#senhaPrioridade");
     var ultimaSenha = $("#ultimaSenhaNumero");
+    var ultimaSenha2 = $("#ultimaSenhaNumero2");
+    var ultimaSenha3 = $("#ultimaSenhaNumero3");
+    var ultimaSenha4 = $("#ultimaSenhaNumero4");
+    var ultimaSenha5 = $("#ultimaSenhaNumero5");
+    var ultimaSenha6 = $("#ultimaSenhaNumero6");
+
     var audioChamada = $("#audioChamada");
 
     if (e.keyCode == 39) {
       // 39 é a seta para a direita
       // 1005 é o limite, ajustar para necessidade
       if (senhaAtual.val() < 1005) {
-        ultimaSenha.html(senhaAtual.html());
         senha = parseInt(senhaNormal.val()) - 1;
         senhaAtual.html(pad(senha, 4));
         senhaNormal.val(pad(senha, 4));
         audioChamada.trigger("play");
+        ultimaSenha.val(senha + 1);
+        ultimaSenha.html(senha + 1);
+        ultimaSenha2.html(Number(pad(ultimaSenha.val(), 4)) + 1);
+        ultimaSenha2.val(Number(pad(ultimaSenha.val(), 4)) + 1);
+        ultimaSenha3.html(Number(pad(ultimaSenha2.val(), 4)) + 1);
+        ultimaSenha3.val(Number(pad(ultimaSenha2.val(), 4)) + 1);
+        ultimaSenha4.html(Number(pad(ultimaSenha3.val(), 4)) + 1);
+        ultimaSenha4.val(Number(pad(ultimaSenha3.val(), 4)) + 1);
+        ultimaSenha5.html(Number(pad(ultimaSenha4.val(), 4)) + 1);
+        ultimaSenha5.val(Number(pad(ultimaSenha4.val(), 4)) + 1);
+        ultimaSenha6.html(Number(pad(ultimaSenha5.val(), 4)) + 1);
+        ultimaSenha6.val(Number(pad(ultimaSenha5.val(), 4)) + 1);
       }
     }
     if (e.keyCode == 37) {
